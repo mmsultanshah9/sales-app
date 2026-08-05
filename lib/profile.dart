f2550f23a9125e263cb8c3c865b2f2cada872a5f
sultan shah <mmsultanshah9@gmail.com>
2026-08-05 14:28:09 +0500

    sales app

--- lib/profile.dart
+++ lib/profile.dart
@@ -6,52 +6,131 @@ class Profilepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 140,
-            color:Colors.blue,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: CircleAvatar(
-                    radius: 60,
-                    backgroundImage: AssetImage("assets/images/images.png"),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Container(
+              height: 140,
+              color: Colors.blue,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: CircleAvatar(
+                      radius: 60,
+                      backgroundImage: AssetImage("assets/images/images.png"),
+                    ),
                   ),
-                ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(child: Container(color: Colors.blueGrey, height: 100)),
+                SizedBox(width: 10),
+                Expanded(child: Container(color: Colors.blueGrey, height: 100)),
+                SizedBox(width: 10),
+                Expanded(child: Container(color: Colors.blueGrey, height: 100)),
               ],
             ),
-          ),
-          SizedBox(height: 10,),
-          Row(
-            
-            children: [
-              SizedBox(width: 10,),
-              Expanded(
-                child: Container(
-                  color: Colors.blueGrey,
-                  height: 100,
+            SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              height: 40,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "how are you",
+                  style: TextStyle(color: Colors.black),
                 ),
               ),
-              SizedBox(width: 10,),
-              Expanded(
-                child: Container(
-                  color: Colors.blueGrey,
-                  height: 100,
+            ),
+                        SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              height: 40,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "how are you",
+                  style: TextStyle(color: Colors.black),
                 ),
               ),
-              SizedBox(width: 10,),
-              Expanded(
-                child: Container(
-                  color: Colors.blueGrey,
-                  height: 100,
+            ),
+                        SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              height: 40,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "how are you",
+                  style: TextStyle(color: Colors.black),
                 ),
               ),
-                SizedBox(width: 10,),
-            ],
-          )
-        ],
+            ),
+                        SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              height: 40,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "how are you",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+                        SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              height: 40,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "how are you",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusGeometry.circular(20),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "logout",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+          ],
+        ),
       ),
     );
   }
